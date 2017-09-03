2470892eafcd8f82b3d971444d13d6fc0dbead49
nekomarino <nekomarino@gmail.com>
2017-09-03 21:52:21 +0800

    added support for   maps

--- app/src/main/java/com/iandanico/tipapplication/Navigate.java
+++ app/src/main/java/com/iandanico/tipapplication/Navigate.java
@@ -1,11 +1,19 @@
 package com.iandanico.tipapplication;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by nekomarino on 9/3/17.
@@ -13,10 +21,14 @@ import android.view.ViewGroup;
 
 public class Navigate extends Fragment {
     View myView;
+    GoogleMap map;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.navigate, container, false);
+
         return myView;
+
     }
+
 }
--- app/src/main/java/com/iandanico/tipapplication/WhereAmI.java
+++ app/src/main/java/com/iandanico/tipapplication/WhereAmI.java
@@ -1,23 +1,45 @@
 package com.iandanico.tipapplication;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by nekomarino on 9/3/17.
  */
 
-public class WhereAmI extends Fragment {
+public class WhereAmI extends Fragment implements OnMapReadyCallback {
     View myView;
+    GoogleMap map;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.where_am_i, container, false);
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager()
+                .findFragmentById(R.id.map1);
+        mapFragment.getMapAsync(this);
+
         return myView;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+        LatLng pp = new LatLng(14.651646, 121.106138);
+        MarkerOptions option = new MarkerOptions();
+        option.position(pp).title("Something Cool");
+        map.addMarker(option);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(pp,(float)15));
+
+    }
 }
