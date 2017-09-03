72181d2a9cccfe616fa339d7dd2e3f2bb23c6f1e
nekomarino <nekomarino@gmail.com>
2017-09-03 14:33:40 +0800

    Initial commit

---
+++ app/src/main/java/com/iandanico/tipapplication/About.java
@@ -0,0 +1,22 @@
+package com.iandanico.tipapplication;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by nekomarino on 9/3/17.
+ */
+
+public class About extends Fragment {
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.about, container, false);
+        return myView;
+    }
+}
---
+++ app/src/main/java/com/iandanico/tipapplication/College.java
@@ -0,0 +1,22 @@
+package com.iandanico.tipapplication;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by nekomarino on 9/3/17.
+ */
+
+public class College extends Fragment {
+    View myView;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myView = inflater.inflate(R.layout.college, container, false);
+        return myView;
+    }
+}
