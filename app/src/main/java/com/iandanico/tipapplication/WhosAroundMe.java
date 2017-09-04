6f31cbd3c048e6466cb03eae0e16773d3e158320
nekomarino <nekomarino@gmail.com>
2017-09-05 03:01:30 +0800

    bluetooth integration

--- app/src/main/java/com/iandanico/tipapplication/WhosAroundMe.java
+++ app/src/main/java/com/iandanico/tipapplication/WhosAroundMe.java
@@ -1,61 +1,103 @@
 package com.iandanico.tipapplication;
 
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.ToggleButton;
+import android.support.v4.app.ListFragment;
+
+import com.google.android.gms.internal.ada;
+
+import java.util.ArrayList;
+
+import static android.content.Context.LAYOUT_INFLATER_SERVICE;
+
 
 /**
  * Created by nekomarino on 9/3/17.
  */
 
-public class WhosAroundMe extends Fragment {
-    ToggleButton bluOnOff;
+public class WhosAroundMe extends ListFragment{
+    ListView btList;
+    Switch bluOnOff;
+    IntentFilter filter;
+    boolean btstatus;
+    static ArrayAdapter adapter;
+    static ArrayList<String> mobileArray = new ArrayList<String>();
+    static BluetoothAdapter bluetoothAdapter;
+
+
     View myView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         myView = inflater.inflate(R.layout.whos_around_me, container, false);
-        Switch bluOnOff = (Switch) myView.findViewById(R.id.switch1);
+        adapter = new ArrayAdapter<String>(getActivity(), R.layout.bt_list_view, mobileArray);
+        bluOnOff = (Switch) myView.findViewById(R.id.switch1);
+        setListAdapter(adapter);
 
-        final boolean btstatus = checkIfBluetoothIsOnOrOff();
+        //handle bluetooth intents
+        filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        getActivity().registerReceiver(mReceiver, filter);
+
+        btstatus = checkIfBluetoothIsOnOrOff();
         bluOnOff.setChecked(btstatus);
+
         bluOnOff.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final boolean btstatus = checkIfBluetoothIsOnOrOff();
+                btstatus = checkIfBluetoothIsOnOrOff();
+                mobileArray.add("Starting Discovery");
+                adapter.notifyDataSetChanged();
                 setBluetooth(!btstatus);
-                Log.d("MyTagGoesHere", Boolean.toString(btstatus));
             }
         });
         return myView;
     }
 
-
-
-
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                String deviceName = device.getName();
+                mobileArray.add(deviceName);
+                adapter.notifyDataSetChanged();
+            }
+        }
+    };
 
     public static boolean checkIfBluetoothIsOnOrOff(){
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         return bluetoothAdapter.enable();
     }
-    public static boolean setBluetooth(boolean enable) {
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+    public static void setBluetooth(boolean enable) {
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         boolean isEnabled = bluetoothAdapter.isEnabled();
         if (enable && !isEnabled) {
-            return bluetoothAdapter.enable();
+            bluetoothAdapter.enable();
         }
         else if(!enable && isEnabled) {
-            return bluetoothAdapter.disable();
+            mobileArray.clear();
+            adapter.notifyDataSetChanged();
+            bluetoothAdapter.disable();
         }
-        // No need to change bluetooth state
-        return true;
+
     }
+
 }
