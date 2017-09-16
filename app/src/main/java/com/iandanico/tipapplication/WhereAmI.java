19ae7e90a5f96ddbf67c3ca1561e1beeff7e6eee
nekomarino <nekomarino@gmail.com>
2017-09-16 15:15:17 +0800

    added getting what address you are in

--- app/src/main/java/com/iandanico/tipapplication/WhereAmI.java
+++ app/src/main/java/com/iandanico/tipapplication/WhereAmI.java
@@ -1,11 +1,18 @@
 package com.iandanico.tipapplication;
 
+import android.content.pm.PackageManager;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v13.app.FragmentCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.location.Address;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,14 +20,27 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.PolylineOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 /**
  * Created by nekomarino on 9/3/17.
  */
 
-public class WhereAmI extends Fragment implements OnMapReadyCallback {
+public class WhereAmI extends Fragment implements GoogleMap.OnMyLocationButtonClickListener,
+        OnMapReadyCallback,
+        FragmentCompat.OnRequestPermissionsResultCallback  {
     View myView;
-    GoogleMap map;
+    GoogleMap mMap;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+    private boolean mPermissionDenied = false;
+    Geocoder geocoder;
+    List<Address> addresses;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -28,18 +48,52 @@ public class WhereAmI extends Fragment implements OnMapReadyCallback {
         SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager()
                 .findFragmentById(R.id.map1);
         mapFragment.getMapAsync(this);
-
+        geocoder = new Geocoder(getActivity(),Locale.getDefault());
         return myView;
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
-        LatLng pp = new LatLng(14.651646, 121.106138);
-        MarkerOptions option = new MarkerOptions();
-        option.position(pp).title("Something Cool");
-        map.addMarker(option);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(pp,(float)15));
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.setOnMyLocationButtonClickListener(this);
+        enableMyLocation();
+//
 
+
+
+    }
+
+    private void enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Permission to access the location is missing.
+            PermissionUtils.requestPermission(getActivity(), LOCATION_PERMISSION_REQUEST_CODE,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION, true);
+        } else if (mMap != null) {
+            // Access to the location has been granted to the app.
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setCompassEnabled(true);
+            mMap.setBuildingsEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            mMap.getUiSettings().setZoomGesturesEnabled(true);
+
+            
+        }
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        try {
+            addresses = geocoder.getFromLocation(mMap.getMyLocation().getLatitude(), mMap.getMyLocation().getLongitude(), 1);
+            Log.d("WAT", "onMyLocationButtonClick: "+addresses.toString());
+            return false;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
     }
 }
